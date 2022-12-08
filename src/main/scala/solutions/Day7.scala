f3b261324e12da5cbf4a56e748ec480044ab2c70
GoateSo <goatesochai@gmail.com>
2022-12-08 11:22:04 -0600

    Day 7 functional solution

--- src/main/scala/solutions/Day7.scala
+++ src/main/scala/solutions/Day7.scala
@@ -1,38 +1,35 @@
 package solutions
 import utils.Utils.*
-import util.chaining.scalaUtilChainingOps
-import scala.collection.mutable.{ArrayBuffer, HashMap}
 
 class Day7(input : Seq[String]) extends Solution(input):
   // input : cmds
   val tree = input.filterNot(_.startsWith("$ ls"))
     .foldLeft(("./", Map.empty[String, List[Any]])){
-      (acc, x) => x match
+      case ((cdir, map), x) => x match
         case s if s.startsWith("$") => 
-          val ndir = s.drop(2).dropWhile(_ != ' ').strip
+          val ndir = s.substring(5) // drop "$ cd " from start
           (ndir match
             case "/" => "./"
-            case ".." => acc._1.split("/").dropRight(1).mkString("/")
-            case _ => acc._1 ++ "/" ++ ndir
-          , acc._2)
+            case ".." => cdir.split("/").dropRight(1).mkString("/")
+            case _ => cdir ++ "/" ++ ndir
+          , map)
         case s => 
           val (p1, p2) = s.span(!_.isSpaceChar)
-          val newV = if p1 == "dir" then s"${acc._1}/${p2.drop(1)}" else p1.toInt
-          (acc._1, acc._2 + (acc._1 -> (newV :: acc._2.getOrElse(acc._1, Nil))))
+          val newV = if p1 == "dir" then s"${cdir}/${p2.drop(1)}" else p1.toInt
+          (cdir, map + (cdir -> (newV :: map.getOrElse(cdir, Nil))))
     }._2
 
-  val realSizes = HashMap[String, Int]()
   def sum(dir : String): Int =    
     tree.get(dir).get.foldLeft(0)((acc, x) => x match
       case s: String => acc + sum(s)
       case i: Int => acc + i
-    ).tap(realSizes(dir) = _)
+    )
 
-  sum("./")
-  val storages = realSizes.toList.map(_._2).sorted
-  
-  override def run: Any =
+  val rootCost=sum("./")
+  val storages = tree.keySet.toList.map(sum).sorted
+ 
+   override def run: Any =
     storages.filter(_ <= 100000).sum
 
   override def run2: Any =
-    storages.dropWhile(70000000-realSizes("./") + _ <= 30000000).head
+    storages.dropWhile(70000000-sum("./") + _ <= 30000000).head
