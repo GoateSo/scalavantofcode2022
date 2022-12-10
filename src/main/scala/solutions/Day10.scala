78fcfe652efb1feef311e22e3292245ac4d8b37d
GoateSo <goatesochai@gmail.com>
2022-12-10 00:14:15 -0600

    functional day 10 part 2

--- src/main/scala/solutions/Day10.scala
+++ src/main/scala/solutions/Day10.scala
@@ -1,26 +1,37 @@
 package solutions
 import utils.Utils.*
 
-class Day10(input : Seq[String]) extends Solution(input) {
+class Day10(input : Seq[String]) extends Solution(input):
   // hacky; add noop to addx operation to make it 2 cycles
   val is = input flatMap ( v => 
     if v == "noop" then Seq(v) else Seq("noop", v)
   )
   // run the sequence of ops
   val ts = is.scanLeft(1) {
-    case (a, b) => if b == "noop" then a else a + b.drop(5).toInt
+    case (a, b) if b == "noop" => a
+    case (a, b) => a + b.drop(5).toInt
   }
 
   override def run = 
     // 20, 60, 100, 140, 180, 220 = 40x + 20
-    (20 to 220 by 40).map(x => x*ts(x-1)).sum
+    (20 to 220 by 40).map(i => i*ts(i-1)).sum
 
-  override def run2 = 
-    val sb = java.lang.StringBuilder()
+  override def run2 =
     // CRT 40 X 6
-    for i <- 0 until 240 do
-      if (i%40 == 0) then
-        sb.append("\n")
-      sb.append(if Math.abs(ts(i)-i%40) <= 1 then "#" else".")
-    sb.toString.trim
-}
+    (0 until 6).map{ i =>
+      (0 until 40).map{ j => 
+        val ind = i*40+j
+        if Math.abs(ts(ind)-ind%40) <= 1 then '#' else '.'
+      }.mkString
+    } mkString("\n")
+
+
+
+
+  // original imperative code:
+  // val sb = new java.lang.StringBuilder()
+  // for i <- 0 until 240 do
+  //   if (i%40 == 0) then sb.append("\n")
+  //   sb.append(if Math.abs(ts(i)-i%40) <= 1 then "#" else".")
+  // sb.toString.trim
+
