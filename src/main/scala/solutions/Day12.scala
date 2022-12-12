735f6cfd0de71a93c643fe4a727731a4fa278fc5
GoateSo <goatesochai@gmail.com>
2022-12-11 23:56:25 -0600

    clean up solution

--- src/main/scala/solutions/Day12.scala
+++ src/main/scala/solutions/Day12.scala
@@ -2,7 +2,7 @@ package solutions
 import utils.Utils.*
 import scala.collection.mutable.{ListBuffer, ArraySeq, HashSet}
 
-class Day12(input : Seq[String]) extends Solution(input) {
+class Day12(input : Seq[String]) extends Solution(input):
   val ys = input.toArray.map(_.toArray)
   var strt = (0,0)
   var end = (0,0)
@@ -15,39 +15,41 @@ class Day12(input : Seq[String]) extends Solution(input) {
         end = (i,j)
         ys(i)(j) = 'z'
 
-  def bfs(starts : Seq[(Int, Int)]): Int = 
-    var minSteps = Int.MaxValue
-    for s <- starts do
-      val visited = HashSet[(Int,Int)](s)
-      var queue = ListBuffer[(Int,Int)]()
-      queue += s
-      var found = false
-      var steps = 0
-      while !queue.isEmpty do
-        steps += 1
-        val nq = ListBuffer[(Int,Int)]()
-        for (i,j) <- queue do
-          if ((i,j)) == end then
-            found = true
-            minSteps = Math.min(minSteps, steps-1)
-          else
-            for (di,dj) <- Seq((0,1),(0,-1),(1,0),(-1,0)) do
-              val (ni,nj) = (i+di,j+dj)
-              if ni >= 0 && ni < ys.size 
-              && nj >= 0 && nj < ys(ni).size 
-              && ys(ni)(nj)-ys(i)(j) <= 1 && !visited.contains((ni,nj)) then
+  def bfs(start : (Int, Int)): Int = 
+    var steps = 0
+    // perform bfs on graph
+    val visited = HashSet[(Int,Int)](start)
+    var queue = List[(Int,Int)](start)
+    var found = false
+    while !queue.isEmpty do
+      steps += 1
+      found ||= queue.contains(end)
+      queue = if found 
+        then Nil 
+        else queue.flatMap( (i,j) => 
+          List((i+1,j),(i-1,j),(i,j+1),(i,j-1))
+            .filter((ni,nj) => 
+              val ret = 
+                   ni >= 0 && ni < ys.size 
+                && nj >= 0 && nj < ys(ni).size 
+                && ys(ni)(nj)-ys(i)(j) <= 1 
+                && !visited.contains((ni,nj))
+              if ret then 
                 visited += ((ni,nj))
-                nq += ((ni,nj))
-        queue = nq
-        if found then queue.clear()
-    minSteps
+              ret
+            )
+        )
+    // check if smth was actually found instead of the subgraph being exhausted
+    if found then steps else Int.MaxValue
+
+  def simulate(starts : Seq[(Int, Int)]): Int = 
+    starts.map(bfs).min - 1
 
   override def run = 
-    bfs(Seq(strt))
+    simulate(Seq(strt))
 
   override def run2 = 
-    bfs(for 
+    simulate(for 
       i <- 0 until ys.size 
       j <- 0 until ys(i).size if ys(i)(j) == 'a' 
     yield (i,j))
-  }
