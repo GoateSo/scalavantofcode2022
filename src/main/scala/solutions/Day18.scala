a6ef9c3cf3dcccee01ce099fde57b90cf4041428
GoateSo <goatesochai@gmail.com>
2022-12-18 01:33:39 -0600

    node cleanup

--- src/main/scala/solutions/Day18.scala
+++ src/main/scala/solutions/Day18.scala
@@ -1,12 +1,10 @@
 package solutions
 import utils.Utils.*
 import scala.collection.mutable.Stack
-import scala.compiletime.ops.int
+
 class Day18(input : Seq[String], isSample : Boolean = false) extends Solution(input,isSample) {
   val poses = input.map(_.split(",").toSeq.map(_.toInt)).toSet
 
-  write(poses mkString "\n")
-  
   val mx = poses.map(_(0)).max
   val my = poses.map(_(1)).max
   val mz = poses.map(_(2)).max
@@ -25,66 +23,47 @@ class Day18(input : Seq[String], isSample : Boolean = false) extends Solution(in
       IndexedSeq(x,y,z-1)
     )
 
+  var init = 0
+  for x <- mix to mx do
+    for y <- miy to my do
+      for z <- miz to mz do
+        if poses.contains(Seq(x,y,z)) then
+          // check number of non- neighboring nodes, and add  
+          init += neighbors(x,y,z).count(x => !poses.contains(x))
+
+  val grid = Array.fill(mx+1,my+1,mz+1)(0)
+  
+  for Seq(x,y,z) <- poses do
+    grid(x)(y)(z) = 1
+
+  def dfs(x : Int, y : Int, z : Int) : Unit =
+    var stack = Stack[(Int,Int,Int)]()
+    stack.push((x,y,z))
+    while !stack.isEmpty do
+      val (x,y,z) = stack.pop()
+      if grid(x)(y)(z) == 0 then
+        grid(x)(y)(z) = 2
+        for Seq(a,b,c) <- neighbors(x,y,z) 
+          if a >= mix && a <= mx && b >= miy && b <= my && c >= miz && c <= mz 
+          if grid(a)(b)(c) == 0 
+        do stack.push((a,b,c))
   override def run: Any = 
-    // // val vsurf  = 
-    // write(mx, my, mz)
-    // write(mix, miy, miz)
-    var n = 0
-    for x <- mix  to mx do
-      for y <- miy to my do
-        for z <- miz to mz do
-          if poses.contains(Seq(x,y,z)) then
-            // check number of non- neighboring nodes, and add  
-            n += neighbors(x,y,z).count(x => !poses.contains(x))
-    n
+    init
   override def run2: Any = 
-    var n = 0
-    for x <- mix to mx do
-      for y <- miy to my do
-        for z <- miz to mz do
-          if poses.contains(Seq(x,y,z)) then
-            // check number of non- neighboring nodes, and add  
-            n += neighbors(x,y,z).count(x => !poses.contains(x))
-    n
-    val grid = Array.fill(mx+1,my+1,mz+1)(0)
-    
-    
-
-    for Seq(x,y,z) <- poses do
-      grid(x)(y)(z) = 1
-    // find number of islands inside the grid that are wholly contained in the grid
-    // find connected components
-
-    // keep track of each component as a set
-    def dfs(x : Int, y : Int, z : Int) : Unit =
-      var stack = Stack[(Int,Int,Int)]()
-      stack.push((x,y,z))
-      while !stack.isEmpty do
-        val (x,y,z) = stack.pop()
-        if grid(x)(y)(z) == 0 then
-          write(x,y,z,"Aeiou")
-          grid(x)(y)(z) = 2
-          for Seq(a,b,c) <- neighbors(x,y,z) 
-          do
-            if a >= mix && a <= mx && b >= miy && b <= my && c >= miz && c <= mz &&
-              grid(a)(b)(c) == 0 then
-              stack.push((a,b,c))
-    for x <- 0 to mx do
-      for y <- 0 to my do
-        for z <- 0 to mz do
-          if x <= mix || y <= miy || z <= miz || x == mx || y == my || z == mz then
-            if grid(x)(y)(z) == 0 then
-              dfs(x,y,z)
-
-    for x <- mix to mx do
-      for y <- miy to my do
-        for z <- miz to mz do
-          if grid(x)(y)(z) == 0 then
-            write(x,y,z,"aeiou")
-            // check number of non- neighboring nodes, and add  
-            n -= neighbors(x,y,z).count{ case Seq(x,y,z) => 
-              x >= mix && x <= mx && y >= miy && y <= my && z >= miz && z <= mz &&
-              grid(x)(y)(z) == 1 
-            }
-    n
+    var rem = 0
+    for x <- mix to mx 
+        y <- mix to my 
+        z <- mix to mz 
+        if x == mix || y == miy || z == miz || x == mx || y == my || z == mz
+        if grid(x)(y)(z) == 0
+    do dfs(x,y,z)
+    for 
+      x <- mix+1 to mx-1
+      y <- miy+1 to my-1
+      z <- miz+1 to mz-1 
+      if grid(x)(y)(z) == 0
+    do rem += neighbors(x,y,z).count{ case Seq(x,y,z) => 
+        grid(x)(y)(z) == 1 
+      }
+    init-rem
 }
